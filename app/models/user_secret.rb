7ffd53a15dbd2c141c2e338e8fb273067bbfd05a
Manish <joharimani@gmail.com>
2017-03-17 18:07:45 +0530

    Create random secret codes.

--- app/models/user_secret.rb
+++ app/models/user_secret.rb
@@ -1,3 +1,15 @@
 class UserSecret < ActiveRecord::Base
   validates :secret_code, presence: true, uniqueness: true
+  before_validation :set_secret, on: :create
+
+  def retrieve_secret_code
+    loop do
+      token = SecureRandom.hex(3)
+      break token unless UserSecret.exists?(secret_code: token)
+    end
+  end
+
+  def set_secret
+    self.secret_code = retrieve_secret_code
+  end
 end
